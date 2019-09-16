36202564ae1e5e8cf2ac84e0617eea1d491f4235
Jhonatas Alves <jhonatasa_s@hotmail.com>
2019-09-16 19:14:33 -0300

    Starting to implement Board an Piece

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,12 +1,12 @@
 package aplication;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Position ps = new Position(3, 5);
-		System.out.println(ps);
+		Board board = new Board(8, 8);
+		
 	}
 
 }
